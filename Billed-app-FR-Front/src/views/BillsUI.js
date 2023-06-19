1f74022eb78b9e2f2204bc3ad4ba9d0d8af501c0
Gautier Laurent <gautierlaurent47@gmail.com>
2023-06-19 21:40:10 +0200

    bug report Bills && bug report Login

---
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -0,0 +1,86 @@
+import VerticalLayout from "./VerticalLayout.js";
+import ErrorPage from "./ErrorPage.js";
+import LoadingPage from "./LoadingPage.js";
+import Actions from "./Actions.js";
+
+const row = (bill) => {
+  return `
+    <tr>
+      <td>${bill.type}</td>
+      <td>${bill.name}</td>
+      <td>${bill.date}</td>
+      <td>${bill.amount} €</td>
+      <td>${bill.status}</td>
+      <td>
+        ${Actions(bill.fileUrl)}
+      </td>
+    </tr>
+    `;
+};
+
+const rows = (data) => {
+  let retrieveData;
+  const regexDate =
+    /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i;
+
+  retrieveData = data.map((date) => date.date);
+
+  const antiChrono = (a, b) => (a < b ? 1 : -1);
+  const datesSorted = [...retrieveData].sort(antiChrono);
+  // console.log("retrieveData", retrieveData, "datesSorted", datesSorted);
+  return data && data.length ? data.map((bill) => row(bill)).join("") : "";
+};
+
+export default ({ data: bills, loading, error }) => {
+  const modal = () => `
+    <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Justificatif</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  if (loading) {
+    return LoadingPage();
+  } else if (error) {
+    return ErrorPage(error);
+  }
+
+  return `
+    <div class='layout'>
+      ${VerticalLayout(120)}
+      <div class='content'>
+        <div class='content-header'>
+          <div class='content-title'> Mes notes de frais </div>
+          <button type="button" data-testid='btn-new-bill' class="btn btn-primary">Nouvelle note de frais</button>
+        </div>
+        <div id="data-table">
+        <table id="example" class="table table-striped" style="width:100%">
+          <thead>
+              <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Date</th>
+                <th>Montant</th>
+                <th>Statut</th>
+                <th>Actions</th>
+              </tr>
+          </thead>
+          <tbody data-testid="tbody">
+            ${rows(bills)}
+          </tbody>
+          </table>
+        </div>
+      </div>
+      ${modal()}
+    </div>`;
+};
